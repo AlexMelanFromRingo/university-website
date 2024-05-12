09182ec617dab6f69968d351c9f261ff61a15e78
Alex Melan <59925151+AlexMelanFromRingo@users.noreply.github.com>
2024-05-12 12:24:57 +0300

    Add ajax
    Test php

--- js/main.js
+++ js/main.js
@@ -19,7 +19,29 @@ $(document).ready(function(){
     $(".myButton").click(function(){
         var filename = $(this).data('file');
         $.ajax({url: filename, success: function(result){
-            $("#content").html(result);
+            $(".desc p").html(result);
         }});
+    });
+});
+
+$(document).ready(function(){
+    $("#loadData").click(function(){
+        $.ajax({
+            url: "http://localhost/data.php",
+            type: "GET",
+            success: function(result){
+                // Очищуємо таблицю, залишаючи тільки заголовок
+                $("#dataTable").find("tr:gt(0)").remove();
+                // Додаємо нові рядки з даними
+                $.each(result.data, function(i, item){
+                    var row = $("<tr>").append(
+                        $("<td>").text(item.id),
+                        $("<td>").text(item.name),
+                        $("<td>").text(item.date)
+                    );
+                    $("#dataTable").append(row);
+                });
+            }
+        });
     });
 });
